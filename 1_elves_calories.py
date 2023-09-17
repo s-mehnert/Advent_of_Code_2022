3c09223419a82f2519e75b55d2b411618adaed04
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2023-09-17 16:50:17 +0200

    fix logical error in with open statement

--- 1_elves_calories.py
+++ 1_elves_calories.py
@@ -12,8 +12,11 @@ with open("1_elves_calories_input.txt") as input:
             cals_per_elf = 0
         else:
             cals_per_elf += int(line.strip("\n"))
+    imported_data.append(cals_per_elf)
 
 
 max_cal_elf = max(imported_data)
 
 print(f"The elf carrying the most calories is carrying {max_cal_elf} calories.")
+
+
