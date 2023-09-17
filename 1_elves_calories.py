60342a480b4cb64dfe16387b708bdb314780707c
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2023-09-17 16:34:34 +0200

    import and format data

---
+++ 1_elves_calories.py
@@ -0,0 +1,17 @@
+#***** ADVENT OF CODE 2022 *****
+#************ DAY 1 *************
+#****************** Part 1 *****
+
+imported_data = list()
+
+with open("1_elves_calories_input.txt") as input:
+    cals_per_elf = 0
+    for line in input.readlines():
+        if line == "\n":
+            imported_data.append(cals_per_elf)
+            cals_per_elf = 0
+        else:
+            cals_per_elf += int(line.strip("\n"))
+
+print(imported_data)
+
