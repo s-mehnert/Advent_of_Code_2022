5f72ee90177d39c8aa0bf56fde035280f00d7f3e
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2023-09-18 18:52:04 +0200

    define function to return faulty item and calculate priority sum

--- 03_rucksack.py
+++ 03_rucksack.py
@@ -4,4 +4,18 @@ with open("03_rucksack_input.txt") as input:
     for line in input.readlines():
         imported_data.append(line.strip("\n"))
 
-print(imported_data)
+
+def find_faulty_item(rucksack):
+    for char in rucksack[:len(rucksack)//2]:
+        if char in rucksack[len(rucksack)//2:]:
+            return char
+
+# letters in string starting with * so that index can directly be used as corresponding value
+letters = "*abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+faulty_characters = list()
+
+for rucksack in imported_data:
+    faulty_characters.append(find_faulty_item(rucksack))
+
+print(f"The priorities of the faulty items in all rucksacks together is {sum([letters.index(char) for char in faulty_characters])}.")
