752bfe9093fe9d05d847dea32b1c3fdc4bc07ac3
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2023-09-19 12:36:19 +0200

    define function to process all instructions at once

--- 05_supply.py
+++ 05_supply.py
@@ -12,13 +12,23 @@ with open("05_supply_input.txt") as input:
 
 
 stacks_raw_data = imported_data[0].split("\n")
-instructions = imported_data[1].split("\n")
-
 stack_rows = list()
+
 for row in stacks_raw_data:
-    stack_rows.append([row[1], row[5], row[9]])
+    items = list()
+    for i in range(1, len(row)-1, 4):
+        items.append(row[i])
+    stack_rows.append(items)
 stack_rows.pop()
 stack_rows.reverse()
+
+instructions = imported_data[1].split("\n")
+formatted_instructions = list()
+
+for row in instructions:
+    instr = row.split()
+    formatted_instructions.append([int(instr[1]), int(instr[3]), int(instr[5])])
+
 
 def build_stacks(rows_bottom_up):
     stack_list = list()
@@ -31,25 +41,27 @@ def build_stacks(rows_bottom_up):
             stack_list[i].append(row[i])
     return stack_list
 
-initial_stacks = build_stacks(stack_rows)
-
-formatted_instructions = list()
-
-for row in instructions:
-    instr = row.split()
-    formatted_instructions.append([int(instr[1]), int(instr[3]), int(instr[5])])
-
-print(formatted_instructions)
-
 def move_item(stack_list, from_stack, to_stack):
     stack_list[to_stack-1].append(stack_list[from_stack-1].pop())
     return stack_list
-print(initial_stacks)
-print(move_item(initial_stacks, 1, 2))
 
-def process_instructions(stacks, instruction):
-    pass
+def process_instructions(stacks, instructions):
+    for instruction in instructions:
+        for i in range(instruction[0]):
+            move_item(stacks, instruction[1], instruction[2])
+    return stacks
 
+def decipher_message(stacks):
+    message = ""
+    for stack in stacks:
+        message += stack[-1]
+    return message
+
+
+initial_stacks = build_stacks(stack_rows)
+stacks_after_rearrangement = process_instructions(initial_stacks, formatted_instructions)
+
+print(f"The message is {decipher_message(stacks_after_rearrangement)}.")
 
 
 
