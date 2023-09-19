da80ea91530c4da8065a162ef1d1abe99b45b503
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2023-09-19 11:28:35 +0200

    Day 5: fetch instructions and import data

---
+++ 05_supply.py
@@ -0,0 +1,17 @@
+#***** ADVENT OF CODE 2022 *****
+#************ DAY 5 ************
+#****************** Part 1 *****
+
+imported_data = list()
+
+with open("05_supply_input.txt") as input:
+    for line in input.readlines():
+        imported_data.append(line)
+
+print(imported_data)
+
+
+
+
+
+#****************** Part 2 *****
