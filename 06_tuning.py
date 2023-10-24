4df964b5ed748d692f391e0a069b911d868bcf03
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2023-10-24 19:37:31 +0200

    Day &: set up project, fetch instructions, import and format data

---
+++ 06_tuning.py
@@ -0,0 +1,10 @@
+#***** ADVENT OF CODE 2022 *****
+#************ DAY 6 ************
+#****************** Part 1 *****
+
+imported_data = ""
+
+with open("06_tuning_input.txt") as input:
+    imported_data = input.read().strip("\n")
+
+print(imported_data)
