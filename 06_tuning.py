62c533aacacd979c694586a331ed67b75f08dee6
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2023-10-24 20:00:54 +0200

    create function to find position in data stream after receiving start-of-packet marker

--- 06_tuning.py
+++ 06_tuning.py
@@ -7,4 +7,19 @@ imported_data = ""
 with open("06_tuning_input.txt") as input:
     imported_data = input.read().strip("\n")
 
-print(imported_data)
+
+def find_pos_start_of_packet_marker(datastream):
+    start_of_packet_marker = list()
+    num_chars_processed = 0
+    for char in datastream:
+        if char in start_of_packet_marker:
+            start_idx = start_of_packet_marker.index(char) +1
+            start_of_packet_marker = start_of_packet_marker[start_idx:]
+        start_of_packet_marker.append(char)
+        num_chars_processed += 1
+        if len(start_of_packet_marker) == 4:
+            return num_chars_processed
+
+pos_start_of_packet_marker = find_pos_start_of_packet_marker(imported_data)
+
+print(f"The start-of-packet marker is found after processing {pos_start_of_packet_marker} characters in the datastream.")
