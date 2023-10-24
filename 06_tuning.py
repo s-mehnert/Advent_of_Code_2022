0d0d3b05c7fc02032ec062ab4c530aaec6049df3
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2023-10-24 20:06:25 +0200

    create function to find position in data stream after receiving start-of-message marker

--- 06_tuning.py
+++ 06_tuning.py
@@ -23,3 +23,22 @@ def find_pos_start_of_packet_marker(datastream):
 pos_start_of_packet_marker = find_pos_start_of_packet_marker(imported_data)
 
 print(f"The start-of-packet marker is found after processing {pos_start_of_packet_marker} characters in the datastream.")
+
+
+#****************** Part 2 *****
+
+def find_pos_start_of_message_marker(datastream):
+    start_of_message_marker = list()
+    num_chars_processed = 0
+    for char in datastream:
+        if char in start_of_message_marker:
+            start_idx = start_of_message_marker.index(char) +1
+            start_of_message_marker = start_of_message_marker[start_idx:]
+        start_of_message_marker.append(char)
+        num_chars_processed += 1
+        if len(start_of_message_marker) == 14:
+            return num_chars_processed
+
+pos_start_of_message_marker = find_pos_start_of_message_marker(imported_data)
+
+print(f"The start-of-message marker is found after processing {pos_start_of_message_marker} characters in the datastream.")
