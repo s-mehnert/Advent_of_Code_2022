cd1d92c2267a9237c26a234a368f52fa5a9e784f
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2023-11-11 17:49:11 +0100

    loop through directories and add them to dictionary

--- 07_file_system.py
+++ 07_file_system.py
@@ -8,5 +8,32 @@ with open("07_file_system_input.txt") as input:
     for line in input.readlines():
         imported_data.append(line.strip("\n"))
 
-for item in imported_data:
-    print(item)
+# for item in imported_data:
+#     print(item)
+
+
+directories = dict()
+files = dict()
+visited = list()
+current_dir = ""
+
+for line in imported_data:
+    if line.startswith("$"):
+        next = "command"
+        command = line[2:4]
+        if command == "cd":
+            dir = line[5:]
+            if dir == "..":
+                visited.pop()
+                current_dir = visited[-1]
+                print("...moving up...")
+            else:
+                visited.append(dir)
+                current_dir = dir
+                if dir not in directories:
+                    directories[dir] = list()
+            print("current directory:", current_dir)
+
+print(directories)
+
+    
